6e619038844aab6af2d79265e5b61f64af56cbe6
vladlol001 <yakavitskiy001@mail.ru>
2021-03-18 11:19:08 +0300

    adaptive design  + grid

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,21 +1,27 @@
 import Search from '../../components/Search';
 import FilmCard from './FilmCard/FilmCard';
-import sMod from './Main.module.css';
 
-function Main(props){
-  // let filmCards = props.movies.map( film => <FilmCard key={film.imdbID} {...film}/>)
-
-  return (
-    <main className={sMod.main}>
-      <div className='container'>
-        <div><Search movieUpdate={props.movieUpdate}/></div>
-        <div className={sMod.main__container}>
-          {props.movies ? 
-          props.movies.map( film => <FilmCard key={film.imdbID} {...film}/>): <div style={{margin: '30px auto', fontSize: '30px'}}>Dont't have films with such name</div>} 
-        </div> 
-      </div>
-    </main>
-  )
+function Main(props) {
+    return (
+        <main className="main">
+            <div className="container">
+                <div>
+                    <Search movieUpdate={props.movieUpdate} />
+                </div>
+                <div className="main__container">
+                    {props.movies ? (
+                        props.movies.map((film) => (
+                            <FilmCard key={film.imdbID} {...film} />
+                        ))
+                    ) : (
+                        <div style={{ margin: '30px auto', fontSize: '30px' }}>
+                            Dont't have films with such name
+                        </div>
+                    )}
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default Main;
